8f2ee3b1ccd60ff0b604c475e788861f2d1f025c
youtpout <eddyboughioul@hotmail.fr>
2024-05-19 22:08:22 +0200

    add client

---
+++ SuiNet.Core/Client/SuiClient.cs
@@ -0,0 +1,20 @@
+﻿using SuiNet.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuiNet.Core.Client
+{
+    public class SuiClient
+    {
+
+        private readonly IHttpTransport _transport;
+
+        public SuiClient(IHttpTransport transport)
+        {
+            this._transport = transport;
+        }
+
+
+    }
+}
